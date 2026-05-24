a774433f4654c3476cf50fe0d850f55a9b63426c
notweary <weary10@outlook.com>
2026-05-24 01:45:21 -0700

    fixed commit

--- wcat.c
+++ wcat.c
@@ -1,8 +1,10 @@
-/*author: Israel Alvarez
+/*
+author: Israel Alvarez
 Custom cat program
 Constraints: fopen(), fgets(), and fclose()
 Can take multiple arguments
-works on very simple text files, as far as i know/(have tried)*/
+works on very simple text files, as far as i know/(have tried)
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
